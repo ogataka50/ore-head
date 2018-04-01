a9654f2061e3beea8edd9cd8fd18e869c34401d1
ogataka50 <ogataka50@gmail.com>
2018-04-01 21:48:45 +0900

    apply gofmt

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
- "flag"
- "fmt"
- "os"
- "bufio"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-    optNum := flag.Int("n", 1, "output num")
+	optNum := flag.Int("n", 1, "output num")
 
-    flag.Parse()
+	flag.Parse()
 
-    fmt.Println("n : ", *optNum)
-    fmt.Println("files:", flag.Args())
+	fmt.Println("n : ", *optNum)
+	fmt.Println("files:", flag.Args())
 
-    for _, v := range flag.Args() {
-        file, err := os.Open(v)
-        if err != nil {
-            panic(err)
-        }
-        defer file.Close()
+	for _, v := range flag.Args() {
+		file, err := os.Open(v)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
 
-        sc := bufio.NewScanner(file)
+		sc := bufio.NewScanner(file)
 
-        fmt.Printf("%s\n", file.Name())
-        //TODO nをintにしたい
-        for i := 1; sc.Scan() && i <= 2 ; i++ {
-            if err := sc.Err(); err != nil {
-                break
-            }
-            fmt.Printf("%s\n", sc.Text())
-        }
-    }
+		fmt.Printf("%s\n", file.Name())
+		//TODO nをintにしたい
+		for i := 1; sc.Scan() && i <= 2; i++ {
+			if err := sc.Err(); err != nil {
+				break
+			}
+			fmt.Printf("%s\n", sc.Text())
+		}
+	}
 }
