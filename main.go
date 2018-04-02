5e2a771bf46edfbaf64a3f2d83521f3e72f6fae8
ogataka50 <ogataka50@gmail.com>
2018-04-02 23:45:15 +0900

    update

--- main.go
+++ main.go
@@ -8,31 +8,39 @@ import (
 )
 
 func main() {
-	optNum := flag.Int("n", 1, "output num")
+	optNum := flag.Int("n", 5, "output line num")
 
 	flag.Parse()
 
-	fmt.Println("n : ", *optNum)
-	fmt.Println("files:", flag.Args())
-
-	// execute
 	for _, v := range flag.Args() {
-		file, err := os.Open(v)
-		if err != nil {
-			panic(err)
-		}
-		defer file.Close()
-
-		sc := bufio.NewScanner(file)
-
-		fmt.Printf("%s\n", file.Name())
-		//TODO nをintにしたい
-		for i := 1; sc.Scan() && i <= 2; i++ {
-			if err := sc.Err(); err != nil {
-				break
-			}
-			fmt.Printf("%s\n", sc.Text())
-		}
-		fmt.Print("\n")
+		hp := HeadPrinter{path: v, n: *optNum, isPrintName: len(flag.Args()) > 1}
+		hp.printHead()
 	}
+}
+
+type HeadPrinter struct {
+	path        string
+	n           int
+	isPrintName bool
+}
+
+func (hp *HeadPrinter) printHead() {
+	file, err := os.Open(hp.path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	if hp.isPrintName {
+		fmt.Printf("==> %s <==\n", file.Name())
+	}
+
+	sc := bufio.NewScanner(file)
+	for i := 1; sc.Scan() && i <= hp.n; i++ {
+		if err := sc.Err(); err != nil {
+			break
+		}
+		fmt.Printf("%s\n", sc.Text())
+	}
+	fmt.Print("\n")
 }
